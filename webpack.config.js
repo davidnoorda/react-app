7c758e6ec84b43434f556c65ab47144a8a9d5dae
David Noorda <david.noorda@learningseat.com>
2020-08-19 21:38:00 +1000

    Add development server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./public",
   },
   module: {
     rules: [
