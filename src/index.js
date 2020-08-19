fc34e984db761019d12152c9f6a7187d11439798
David Noorda <david.noorda@sap.com>
2020-08-19 10:28:27 +1000

    Bundle css files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import App from "./app";
+import "./style.css";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = App();
+  element.classList.add("red");
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,12 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
